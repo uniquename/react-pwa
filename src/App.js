c2a26a4f19b78327d4390a1b54ecefe1edf0d429
Hendrik Kuck <h@12deg.de>
2017-10-18 16:52:36 +0200

    eject and https://github.com/facebookincubator/create-react-app/pull/2714/commits/c566e734f2e36c952b9239e5b8af5623c2e66859

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
         <p className="App-intro">
-          To get started, asdedit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
